6e1d60c966ba9d40e6e833dbdbfc0210f64fc411
Ryan Dome <ryan.dome@centre.edu>
2019-10-15 10:39:42 -0400

    changes

--- code.js
+++ code.js
@@ -1,1 +1,41 @@
+d3.json("https://restcountries.eu/rest/v2/all")
 
+var countryPromise=d3.json("https://restcountries.eu/rest/v2/all");
+countryPromise.then(
+function(data)
+    {
+        //cImage(data);
+        cList(data);
+        console.log("works",data);
+    },
+    function(err)
+    {
+        console.log("error",data);
+    })
+
+
+var cList = function(data)
+{
+   d3.select("body").append("ul").attr("id","list");
+    d3.select("#list").selectAll("li")
+    .data(data)
+    .enter()
+    .append("li")
+    .append("div")
+    .attr("id","countryNames")
+    .text(function(d){
+        return d.name
+    })
+    
+    .append("div").attr("id","flags").append("img").attr("src",function(d){return d.flag}).attr("height",100).attr("width",200);
+}
+
+/*var cImage = function(data)
+{
+    d3.select("#countryNames").append("div").attr("id","flags").append("svg").attr("src",function(d){return d.flag});
+   
+    
+    
+    
+    
+}*/
